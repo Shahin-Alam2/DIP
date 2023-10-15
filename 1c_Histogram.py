9477f71f52e9531f0d9fecedfd13d9ad80429ca4
Shahin Alam <43540136+Shahin-Alam2@users.noreply.github.com>
2023-10-16 01:20:07 +0600

    Add files via upload

---
+++ 1c_Histogram.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_gray_image(image):
+    gray_image = np.mean(image, axis=2).astype(np.uint8)
+    return gray_image
+
+
+def histogram_generate(image):
+    pixel_counts = np.zeros(256, dtype=int)
+    for row in image:
+        for pixel_value in row:
+            pixel_counts[pixel_value] += 1
+
+    return pixel_counts
+
+rgb_image = plt.imread('D:\Odd\Image Processing\Lab\myself\images/cat.jpg')
+gray_image = make_gray_image(rgb_image)
+
+pixel_counts = histogram_generate(gray_image)
+# print(pixel_counts)
+
+
+threshold = 100
+segmented_image = (gray_image > threshold).astype(np.uint8)*255
+# print(segmented_image)
+pixel_counts2 = histogram_generate(segmented_image)
+
+plt.figure(figsize=(8, 7))
+plt.subplot(2,2,1)
+plt.imshow(gray_image, cmap='gray')
+plt.title('Original')
+plt.subplot(2,2,2)
+plt.bar(range(256), pixel_counts)
+plt.title('Histogram 1')
+plt.subplot(2,2,3)
+plt.imshow(segmented_image, cmap='gray')
+plt.title(f'binary Threshold:{threshold}')
+plt.subplot(2,2,4)
+plt.bar(range(256), pixel_counts2)
+plt.title('Histogram 2')
+
+plt.tight_layout()
+plt.show()
